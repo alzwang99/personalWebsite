2c3c836c4e8dcefa5f6b8eb83aaf9b35c5ee740b
alzwang99 <alzwang99@gmail.com>
2024-05-12 01:06:36 -0400

    added splash animation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,21 +3,41 @@ import About from "../components/About"
 import Section from "../components/Section"
 import resumeData from "../data/resumeData"
 import projectData from "../data/projectData"
+import { Example } from "../components/LogoAnimation"
+import { useState, useEffect } from "react";
+
+
 
 function Home() {
+
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => setLoaded(true), 6000)
+    }, []);
+
+
     return (
-        <div className=" pt-12">
-            <div className="w-5/8 tb:w-3/8 tb:float-left tb:fixed z-1">
-                <About />
-            </div>
-            <div className="w-full tb:w-2/4 tb:float-right z-2">
-                <Section data={resumeData} contentType="Resume" />
-                <Section data={projectData} contentType="Projects" />
-                <div className="max-w-[33rem] tb:max-w-[40rem] p-10 m-auto tb:text-start text-center">
-                    <p>Designed with React and Tailwind.css</p>
+        <>
+            {loaded === false ?
+                <div className="svg-animation splash-fade">
+                    <Example />
+                    <p className="splash-text">alzwang.com</p>
+                </div> :
+                <div className="pt-12 content-fade">
+                    <div className="w-5/8 tb:w-3/8 tb:float-left tb:fixed z-1">
+                        <About />
+                    </div>
+                    <div className="w-full tb:w-2/4 tb:float-right z-2">
+                        <Section data={resumeData} contentType="Resume" />
+                        <Section data={projectData} contentType="Projects" />
+                        <div className="max-w-[33rem] tb:max-w-[40rem] p-10 m-auto tb:text-start text-center">
+                            <p>Designed with React and Tailwind.css</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            }
+        </>
     )
 }
 
